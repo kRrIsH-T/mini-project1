3ac325353d14256937677f1a507582d4840f45d7
krrish-t <krrish.tomar@research.iiit.ac.in>
2025-09-27 15:45:00 +0530

    Add null check malloc

--- shell/src/input.c
+++ shell/src/input.c
@@ -2,11 +2,16 @@
 
 char *read_input() {
     char *line = malloc(COMMAND_MAX);
+    if (line == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
     if (fgets(line, COMMAND_MAX, stdin) == NULL) {
         free(line);
         return NULL;
     }
-    
+
     line[strcspn(line, "\n")] = 0;
     return line;
 }
